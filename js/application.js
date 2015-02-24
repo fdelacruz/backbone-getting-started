a2c09a5fca14e6a43e19f4d0b199b0a5d19267f6
Francisco De La Cruz <helloquico@gmail.com>
2015-02-23 23:39:12 -0500

    Add a View

--- js/application.js
+++ js/application.js
@@ -16,3 +16,20 @@ var Animal = Backbone.Model.extend({
     alert(this.get('name') + ' is sleeping.');
   }
 });
+
+var AnimalView = Backbone.View.extend({
+  tagName: 'li', // defaults to div if not specified
+  className: 'animal', // optional, can also set multiple like 'animal dog'
+  id: 'dogs', // also optional
+  events: {
+    'click':         'alertTest',
+    'click .edit':   'editAnimal',
+    'click .delete': 'deleteAnimal'
+  },
+  initialize: function() {
+    this.render(); // render is an optional function that defines the logic for rendering a template
+  },
+  render: function() {
+    this.$el.html(this.model.get('name') + ' is ' + this.model.get('color') + ' and says ' + this.model.get('sound'));
+  }
+});
